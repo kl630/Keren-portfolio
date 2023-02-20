c3cee17cc0365358cd65e43f0ec84339e9cb277a
Keren Liu <karenliu816@gmail.com>
2023-02-19 19:15:27 -0500

    ready to host

--- src/client/components/Projects.jsx
+++ src/client/components/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import FindYourSpace from '../../images/FindYourSpace.png';
-import Atelier from '../../images/Atelier.png';
-
+import AtelierVideo from '../../images/AtelierVideo.mov';
 
 const Projects = (() => {
   return (<div className="projects">
@@ -13,8 +12,9 @@ const Projects = (() => {
     </div>
 
     <div className="project-row">
-      {/* TODO:  add live screenshot of the project */}
-      <img className="Atelier-img" src={Atelier} alt="Atelier-img"/>
+      <video className="Atelier-mov" controls autoPlay loop muted>
+        <source src={AtelierVideo} type="video/mp4"></source>
+      </video>
       <h3 onClick={() => window.open('https://github.com/rpp2204-fec-basilseed/Atelier', '_blank')}>Atelier</h3>
       <p>An E-commerce website which allows users to search for questions and answers, ask and answer questions related to a product.</p>
     </div>
