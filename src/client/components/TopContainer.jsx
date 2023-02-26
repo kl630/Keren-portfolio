3054b5e07b1d69ccf39e7c8c449e635ce0df7d0f
Keren Liu <karenliu816@gmail.com>
2023-02-25 19:30:03 -0500

    ready to deploy

--- src/client/components/TopContainer.jsx
+++ src/client/components/TopContainer.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import cloud from '../../images/cloud.png';
 import mountain from '../../images/mountain.png';
 
-
 const TopContainer = (() => {
-  return (<div className="top-container">
+  return (<div className="top-container-parallax">
       <img className="top-cloud" src={cloud} alt="cloud-img" />
       <h1>I'm Keren Liu</h1>
       <h2>a software <span>eng</span>ineer.</h2>
